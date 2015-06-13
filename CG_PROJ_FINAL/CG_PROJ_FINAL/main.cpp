be77774e14697f90d4a20a0ab59019a90868ba56
CGabrielOP <c.gabriel.oliveira.p@gmail.com>
2015-06-13 01:33:14 +0100

    a comecar a camera

--- CG_PROJ_FINAL/CG_PROJ_FINAL/main.cpp
+++ CG_PROJ_FINAL/CG_PROJ_FINAL/main.cpp
@@ -4,19 +4,67 @@
 #include <GL/glut.h>
 #endif
 #include <iostream>
+#include <cmath>
 
 float width=800;
 float height=600;
 
-class Casa{
+class Camera{
     
-    int altura;
-    int comprimento;
-    int largura;
+    double teta;
+    double y;
+    double dTeta;
+    double dY;
     
 public:
     
-    void set_values(int a,int c,int l){
+    Camera(double t,double yp,double dt,double dyp){
+        
+        teta=t;
+        y=yp;
+        dTeta=dt;
+        dY=dyp;
+        
+    }
+    
+    double getX(){
+        return 10 * cos(teta);
+    }
+    
+    double getY(){
+        return y;
+    }
+    
+    double getZ(){
+        return 10 * sin(teta);
+    }
+    
+    void moveRight(){
+        teta += dTeta;
+    }
+    
+    void moveLeft(){
+        teta -= dTeta;
+    }
+    void moveUp(){
+        y += dY;
+    }
+    void moveDown(){
+        if (y>dY)
+            y -= dY;
+    }
+    
+};
+
+class Casa{
+    
+    float altura;
+    float comprimento;
+    float largura;
+    
+public:
+    
+    void set_values(float a,float c,float l){
         
         altura=a;
         comprimento=c;
@@ -30,14 +78,71 @@ public:
     
     void draw(){
         
-        glColor4f(1.0, 1.0, 0.0, 0.0);
+        glColor4f(1.0, 0.0, 0.0, 0.0);
         
         glBegin(GL_QUADS);
         
         glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.0, 0.0, 8.0);
-        glVertex3f(8.0, 0.0, 8.0);
-        glVertex3f(8.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, largura);
+        glVertex3f(comprimento, 0.0, largura);
+        glVertex3f(comprimento, 0.0, 0.0);
+        
+        glEnd();
+        
+        
+        glColor4f(0.0, 1.0, 0.0, 0.0);
+        
+        glBegin(GL_QUADS);
+        
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, altura, 0.0);
+        glVertex3f(comprimento, altura, 0.0);
+        glVertex3f(comprimento, 0.0, 0.0);
+        
+        glEnd();
+        
+        
+        glColor4f(0.0, 0.0, 1.0, 0.0);
+        
+        glBegin(GL_QUADS);
+        
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, altura, 0.0);
+        glVertex3f(0.0, altura, largura);
+        glVertex3f(0.0, 0.0, largura);
+        
+        glEnd();
+        
+        glColor4f(0.0, 0.0, 1.0, 0.0);
+        
+        glBegin(GL_QUADS);
+        
+        glVertex3f(comprimento, 0.0, 0.0);
+        glVertex3f(comprimento, altura, 0.0);
+        glVertex3f(comprimento, altura, largura);
+        glVertex3f(comprimento, 0.0, largura);
+        
+        glEnd();
+        
+        glColor4f(0.0, 1.0, 0.0, 0.0);
+        
+        glBegin(GL_QUADS);
+        
+        glVertex3f(0.0, 0.0, largura);
+        glVertex3f(0.0, altura, largura);
+        glVertex3f(comprimento, altura, largura);
+        glVertex3f(comprimento, 0.0, largura);
+        
+        glEnd();
+        
+        glColor4f(1.0, 0.0, 0.0, 0.0);
+        
+        glBegin(GL_QUADS);
+        
+        glVertex3f(0.0, altura, 0.0);
+        glVertex3f(0.0, altura, largura);
+        glVertex3f(comprimento, altura, largura);
+        glVertex3f(comprimento, altura, 0.0);
         
         glEnd();
         
@@ -70,7 +175,7 @@ void projection(){
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(5.0,25.0,5.0, 0, 0, 0, 0.0, 1.0, 0.0);
+    gluLookAt(8.0,4.0,8.0, 0, 4.0, 0, 0.0, 1.0, 0.0);
     
     /******************************************/
     
@@ -80,6 +185,8 @@ void projection(){
 void init() {
     
     Casa house;
+    
+    house.set_values(8.0, 8.0, 8.0);
     
     projection();
     
